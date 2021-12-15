d51e28e3b2ea632ca6ea0bed0400abcd0a021eea
Leticia <canumsang@gmail.com>
2021-12-15 18:00:23 -0300

    class created without hardcoding and using __init__

--- creating_class_method.py
+++ creating_class_method.py
@@ -2,16 +2,26 @@
 #to be applied on this class.
 
 class Item:
-	def calculate_total_price(self, x, y):
-		return x * y
+	def __init__(self, name: str, price: float, quantity=0):
+		# Run validations to the received arguments
+		assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+		assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
 
-item1 = Item()
+		# Assign to self object
+		self.name = name
+		self.price = price
+		self.quantity = quantity
+
+	def calculate_total_price(self):
+		return self.price * self.quantity
+
+item1 = Item("Phone", 100, 5)
 
 # assigning attributes to the instance of the Item class
 # but this is hardcoding the attributes
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
+# item1.name = "Phone"
+# item1.price = 100
+# item1.quantity = 5
 
 #calling the method on the instance
-print(item1.calculate_total_price(item1.price, item1.quantity));
+print(item1.calculate_total_price());
